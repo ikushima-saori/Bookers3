4909a40324327b40214d0a36746df68e7f75cc84
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-08-06 06:26:07 +0000

    [Add]投稿編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   def edit
+    @book = Book.find(params[:id])
   end
 
   def index
@@ -17,6 +18,12 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     @book.save
     redirect_to book_path(@book)
+  end
+
+  def update
+   @book = Book.find(params[:id])
+   @book.update(book_params)
+   redirect_to book_path(@book.id)
   end
 
   def destroy
